175d14d7be509ad4c60f4256cd1422b240d9db1d
asfuz <ubuntusamandar@gmail.com>
2022-12-20 12:09:03 +0500

    added Answer model

---
+++ backend/database/migrations/2022_12_20_070507_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('test_id')->constrained();
+            $table->boolean('correct')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};
